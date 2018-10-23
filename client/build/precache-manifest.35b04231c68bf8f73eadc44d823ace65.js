9558d564ff296de7ddff4406df8dfce8c5ac0acc
adee <adee4990@gmail.com>
2018-10-23 18:48:41 -0400

    Fixed build

---
+++ client/build/precache-manifest.35b04231c68bf8f73eadc44d823ace65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a677c01eeef0f51ebea",
+    "url": "/static/js/main.8a677c01.chunk.js"
+  },
+  {
+    "revision": "1ff8dd5880b92f76bfab",
+    "url": "/static/js/1.1ff8dd58.chunk.js"
+  },
+  {
+    "revision": "8a677c01eeef0f51ebea",
+    "url": "/static/css/main.ef79efd2.chunk.css"
+  },
+  {
+    "revision": "d3b13f293be0806261c4950a2d742584",
+    "url": "/index.html"
+  }
+];
